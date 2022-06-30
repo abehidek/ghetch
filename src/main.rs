b340eaee7b7dfb74ad64b52fff519e04c054937b
abehidek <hidek.abe@outlook.com>
2022-06-29 23:28:54 -0300

    Add basic args matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::env::{self};
+
+fn fetch_user(username: &str) {
+    println!("[log] fetching user {}", username);
+}
+
+fn fetch_repo(reponame: &str) {
+    println!("[log] fetching repository {}", reponame);
+}
+
+fn match_args(args: Vec<String>) {
+    for (index, arg) in args.iter().enumerate() {
+        match &&arg[..] {
+            &"-r" | &"--repository" => {
+                if args.len() <= index + 1 {
+                    println!("Which repo?");
+                    return;
+                }
+                let reponame = &args[index + 1][..];
+                fetch_repo(reponame);
+                return;
+            }
+            &"-u" | &"--user" => {
+                if args.len() <= index + 1 {
+                    println!("Which user?");
+                    return;
+                }
+                let username = &args[index + 1][..];
+                fetch_user(username);
+                return;
+            }
+            &&_ => {
+                println!("Unknow command");
+                return;
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        match_args(args);
+    } else {
+        println!("commands: ")
+    }
 }
