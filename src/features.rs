93664dd46c72a02b6576eb78aed7de15dcb14d2c
abehidek <hidek.abe@outlook.com>
2022-07-01 18:25:28 -0300

    Simplify the code because difficulties, add api fetch function

--- src/features.rs
+++ src/features.rs
@@ -1,72 +1,36 @@
-use std::process::Output;
+use reqwest::header::{AUTHORIZATION, USER_AGENT};
 
-use futures::Future;
-
-pub struct Feature {
-    pub name: String,
-    pub description: String,
-    pub necessary_args: String,
-    pub flags: Vec<String>,
-    pub function: fn(&str),
+async fn fetch_api(url: String) {
+    println!("[log][fetch_api] fetching api: {}", url);
+    let reqwest_client = reqwest::Client::new();
+    let response = reqwest_client
+        .get(url)
+        .header(USER_AGENT, "rust")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .expect("Fetch failed");
+    print!("Response: {}", response.to_string());
 }
 
-pub fn get() -> Vec<Feature> {
-    vec![
-        Feature {
-            name: "fetch-repo".to_string(),
-            description: "Fetch a github repo".to_string(),
-            necessary_args: "username-reponame or repo url".to_string(),
-            flags: vec![
-                "-r".to_string(),
-                "--repository".to_string(),
-                "--repo".to_string(),
-            ],
-            function: fetch_repo,
-        },
-        Feature {
-            name: "fetch-user".to_string(),
-            description: "Fetch a github user".to_string(),
-            necessary_args: "username or user url".to_string(),
-            flags: vec!["-u".to_string(), "--user".to_string()],
-            function: fetch_user,
-        },
-        Feature {
-            name: "help".to_string(),
-            description: "All commands to help user".to_string(),
-            necessary_args: "".to_string(),
-            flags: vec!["-h".to_string(), "--help".to_string()],
-            function: help,
-        },
-    ]
-}
-
-enum FetchErrors {
-    Failed,
-}
-
-fn fetch_user(username: &str) {
-    println!("[log] fetching user {}", username);
+pub async fn fetch_user(username: &str) {
     /*
         needs to accept both url and "username" formats
         also, needs to count the lines and create a language bar for the user (most used langs)
     */
+    println!("[log] fetching user {}", username);
+
+    let url: String = format!("https://api.github.com/users/{}", username);
+    fetch_api(url).await;
 }
 
-// async fn fetch_api(url: String) {
-//     let reqwest_client = reqwest::Client::new();
-//     let response = reqwest_client
-//         .get(url)
-//         .header(USER_AGENT, "rust")
-//         .send()
-//         .await
-//         .unwrap()
-//         .text()
-//         .await
-//         .expect("Fetch failed");
-//     print!("Response: {}", response.to_string());
-// }
-
-fn fetch_repo(user_arg: &str) {
+pub async fn fetch_repo(user_arg: &str) {
+    /*
+        needs to accept both url and "username-reponame" formats
+        also, needs to count the lines and create a language bar for the repo
+    */
     println!("[log] fetching repository {}", user_arg);
 
     if !user_arg.contains("-") {
@@ -75,11 +39,8 @@ fn fetch_repo(user_arg: &str) {
     };
 
     let vec: Vec<&str> = user_arg.split("-").collect();
-    let url: String = format!("https://api.github.com/users/{}/{}", vec[0], vec[1]);
-    /*
-        needs to accept both url and "username-reponame" formats
-        also, needs to count the lines and create a language bar for the repo
-    */
+    let url: String = format!("https://api.github.com/repos/{}/{}", vec[0], vec[1]);
+    fetch_api(url).await;
 }
 
 pub fn help(_str: &str) {
@@ -87,10 +48,4 @@ pub fn help(_str: &str) {
         "USAGE: ghetch [options]
     \nOPTIONS:"
     );
-    for feature in get() {
-        print!("    {:?}", feature.flags);
-        if !feature.necessary_args.is_empty() {
-            println!(" necessary args: {}", feature.necessary_args)
-        }
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,42 @@
 mod features;
 use std::env;
+use tokio;
 
-fn match_args(args: Vec<String>, features: Vec<features::Feature>) {
+async fn match_args(args: Vec<String>) {
     /*
         need simpliciation and support for easy to add feature
         minimalist version of onefetch
     */
 
     for (arg_index, arg) in args.iter().enumerate() {
-        for feature in features.iter() {
-            // println!("[debug] arg {}, feature {}", arg, feature.name);
-            if feature.flags.contains(arg) {
-                println!("running feature: {}", feature.name);
-                let feature_function: fn(&str) = feature.function;
-
-                if !feature.necessary_args.is_empty() {
-                    if args.len() <= arg_index + 1 {
-                        println!("You need to pass {}", feature.necessary_args);
-                        return;
-                    }
-                    feature_function(&args[arg_index + 1][..]);
-                } else {
-                    feature_function("");
+        match &arg[..] {
+            "-r" | "--repo" | "--repository" => {
+                if !args.len() >= arg_index + 1 {
+                    features::fetch_repo(&args[arg_index + 1][..]).await;
+                    return;
                 }
-                return;
             }
+            "-u" | "--user" => {
+                if args.len() >= arg_index + 1 {
+                    features::fetch_user(&args[arg_index + 1][..]).await;
+                    return;
+                }
+            }
+            _ => {}
         }
     }
     println!("Unknow command");
     return;
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let features: Vec<features::Feature> = features::get();
 
     if args.len() > 1 {
-        match_args(args, features);
+        match_args(args).await;
     } else {
         features::help("");
     }
+    Ok(())
 }
